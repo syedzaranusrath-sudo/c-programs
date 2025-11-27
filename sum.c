9cdd088445194fac13310629ed88e8915954c000
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-11-27 13:25:49 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,sum;
+    printf("enter a value:\n",a);
+        scanf("%d", &a);
+    printf("enter b value:\n",b);
+    scanf("%d", &b);
+    sum=a+b;
+    printf("sum:%d",sum);
+    }
