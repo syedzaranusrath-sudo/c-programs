adc787a96be1e46a089f1631d965fa0f00f0b42b
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-11-27 15:47:54 +0530

    types of variables.c

---
+++ types of variables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int age=20;
+    float height=5.7;
+    char grade='a';
+    printf("age:%d\n",age);
+    printf("height:%f\n",height);
+    printf("grade:%c\n",grade);
+    return 0;
+}
