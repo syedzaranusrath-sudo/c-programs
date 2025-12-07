b3dafe512a9ee180ce6c027efdf1216e48a1a036
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-12-07 16:59:16 +0530

    simple if.c

---
+++ simple if.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int age=0;
+  printf("enter your age:");
+  scanf("%d",&age);
+  if(age>=18)
+  {
+      printf("person is eligiable for vote");
+  }
+  else
+  {
+      printf("person is not eligiable for vote");
+  }
+   return 0;
+}
+
+    
+    
