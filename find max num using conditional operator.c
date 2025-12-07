50b5b733907427425ed045ea56b6f357cb1af58b
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-12-07 16:28:22 +0530

    find max num using conditional operator.c

---
+++ find max num using conditional operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,max;
+    printf("enter any two numbers:");
+    scanf("%d%d",&x,&y);
+    max=x>y?x:y;
+    printf("the maximum number is:%d",max);
+}
+
+    
+    
