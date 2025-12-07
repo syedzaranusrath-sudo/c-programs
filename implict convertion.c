9a110402e8b11446fbd31016ef3ea1ad873e8e3b
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-12-07 16:47:40 +0530

    implict convertion.c
    Demonstrate implicit conversion from int to float in addition.

---
+++ implict convertion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    float b=5.5;
+    double res;
+    res=a+b;
+      printf("integer value %d\n",a);
+      printf("float value %f\n",b);
+      printf("result after implict convertion:%f\n",res);
+   return 0;
+}
+
+    
+    
