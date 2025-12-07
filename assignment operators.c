ccec4d7eccf5b970a2b08a0ab912106e14c956dc
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-12-07 16:18:04 +0530

    assignment operators.c

---
+++ assignment operators.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int x=5;
+    printf("x=%d",x);
+    x+=5;
+    printf("\nx+=5: %d", x);
+    x-=5;
+    printf("\nx-=5: %d", x);
+    x*=5;
+    printf("\nx*=5: %d", x);
+    x/=5;
+    printf("\nx/=5: %d", x);
+    x%=5;
+    printf("\nx%=5: %d", x);
+    return 0;
+    }
