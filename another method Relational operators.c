ee9c8780e21ca841022daf086ec981196c2d5d0c
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-12-07 17:15:45 +0530

    another method Relational operators.c

--- another method Relational operators.c
+++ another method Relational operators.c
@@ -2,14 +2,16 @@
 int main()
 {
     int x,y,a,b,c,d,e,f;
-    printf("enter the values:");
-    scanf("%d%d",&x,&y); 
+    x=2,y=4;
     a=x<y;
     b=x<=y;
     c=x>y;
     d=x>=y;
     e=x==y;
     f=x!=y;
-    printf("the answers of relational operators %d\n%d\n%d\n%d\n%d\n%d\n%",a,b,c,d,e,f);
+    printf("the answers of relational operators\n%d\n%d\n%d\n%d\n%d\n%d\n%",a,b,c,d,e,f);
     return 0;
 }
+
+    
+    
