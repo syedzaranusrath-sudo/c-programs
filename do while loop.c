9d8329133951c7f001b5d9d725c5e7b3b8d86d35
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-12-09 22:16:39 +0530

    Implement do-while loop to print numbers 1 to 100

---
+++ do while loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    printf("natural numbers from 1 to 100:\n");
+    do
+    {
+        printf(" %d",i);
+          i++;
+    }
+    while(i<=100);
+    return 0;
+}
