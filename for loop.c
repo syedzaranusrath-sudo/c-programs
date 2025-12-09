7e6b9eae74d27e6fb6d2b6f83130e1fadfbe8161
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-12-09 22:26:03 +0530

    for loop to print natural numbers from 1 to 500

---
+++ for loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("the natural numbers from 1 to 500\n");
+    for(num=0;num<=500;num++)
+    {
+        printf(" %d",num);
+    }
+    printf("\nloop end");
+    return 0;
+}
