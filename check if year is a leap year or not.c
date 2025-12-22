2060014fd8918dc239c9f4b1d409b9e937313318
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-12-22 14:33:57 +0530

    check if year is a leap year or not.c

---
+++ check if year is a leap year or not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("enter the year:");
+    scanf("%d",&year);
+    if(year%400==0)
+    {
+        printf("%d is a leap year",year);
+    }
+    else
+    {
+        printf("%d is not leap year",year);
+    }
+    return 0;
+}
