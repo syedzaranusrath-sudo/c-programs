d37152d5319c1f994adc549b815ca47deb4d4079
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-12-24 23:26:12 +0530

    find all the roots of quadratic equation1.c

---
+++ find all the roots of quadratic equation1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,b,c,r1,r2,d;
+    printf("enter a,b,c values:");
+    scanf("%f%f%f",&a,&b,&c);
+    d=b*b-4*a*c;
+    if(d==0)
+    {
+        printf("the roots are real and equal\n");
+        r1=-b/(2*a);
+        r2=-b/(2*a);
+        printf("root1=%f anf root2=%f",r1,r2);
+    }
+        else if(d>0)
+        {
+            printf("roots are real and different");
+            r1=(-b+sqrt(d))/(2*a);
+            r2=(-b+sqrt(d))/(2*a);
+            printf("root1=%f",r1);
+            printf("root2=%f",r2);
+        }
+        else
+        {
+            printf("the roos are immaginary");
+        }
+}
