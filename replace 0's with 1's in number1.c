c82d07c80c68182858e229265dae61722c7258f6
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-12-24 12:33:34 +0530

    replace 0's with 1's in number1.c

---
+++ replace 0's with 1's in number1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int num, rem, result = 0, place = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num == 0)
+    {
+        result = 1;
+    }
+   while (num > 0)
+    {
+        rem = num % 10;
+        if (rem == 0)
+            rem = 1;
+        result = rem * place + result;
+        place = place * 10;
+        num = num / 10;
+    }
+   printf("Number after replacing 0 with 1: %d", result);
+    return 0;
+}
