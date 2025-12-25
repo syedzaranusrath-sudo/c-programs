2558c7f0742bada597775618393b0856ffea8338
syedzaranusrath-sudo <syedzaranusrath@gmail.com>
2025-12-25 15:34:24 +0530

    pyramid pattern printing for odd number of stars

---
+++ pyramid pattern print odd num stars2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int rows; 
+    int i, j;
+    printf("enter no of rows:");
+    scanf("%d",&rows);
+    for (i = 1; i <= rows; i++)
+    {
+        for (j = 1; j <= rows - i; j++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= (2 * i - 1); j++)
+        {
+            printf("*");
+        }
+            printf("\n");
+    }
+    return 0;
+}
